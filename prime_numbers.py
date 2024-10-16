6d4102418919e17dc11d4e61df61531582d4c4f6
Ahilandhas <125820870+Ahilandhas@users.noreply.github.com>
2024-10-16 11:33:11 +0530

    Create prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,61 @@
+"""
+Time complexity -> O(n)
+space complexity -> O(1)
+"""
+
+
+def check_prime(num:int):
+    for i in range((num//2)+1):
+         if num % i == 0:
+             return False
+
+    return True
+
+print(check_prime(10))
+
+
+"""
+time complexity -> O(sqaure root(n) )
+space complexity -> O(1)
+"""
+
+def is_prime(n):
+    if n <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+
+    i = 2
+    while i * i <= n:  # Check up to the square root of n
+        if n % i == 0:  # If n is divisible by i, it's not prime
+            return False  # n is not prime
+        i += 1  # Increment i
+    return True  # If no divisors found, n is prime
+
+print(is_prime(49))
+
+"""
+super efficient
+time complexity -> O(sqaure root(n) )
+space complexity -> O(1)
+"""
+
+def is_prime(n):
+    if n <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+
+    if n == 1:
+        return False
+    if n == 2 or n  == 3 :
+        return True
+    if n % 2 == 0  or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:  # Check up to the square root of n
+        if n % i == 0 or n % (i+2)==0:  # If n is divisible by i, it's not prime
+            return False  # n is not prime
+        i += 6  # Increment i
+    return True  # If no divisors found, n is prime
+
+print(is_prime(49))
+
+
